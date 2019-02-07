7e5a22880389a2885dbbe595f5d3a0a84fc62688
Evgenii Rtishchev <e.rtishchev@gmail.com>
2019-02-07 22:31:52 +0300

    Initial Commit

---
+++ ScrollableTextInput/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollableTextInput
+//
+//  Created by Evgenii Rtishchev on 07/02/2019.
+//  Copyright © 2019 Evgenii Rtishchev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
